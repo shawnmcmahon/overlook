af75e9b86fa56ddf9c52835b1ec9abd8652d7657
Shawn M (he/him) <shawnmcmahon17@gmail.com>
2021-06-09 11:20:15 -0600

    Setup hotel test structure

---
+++ test/hotel-test.js
@@ -0,0 +1,25 @@
+import chai from 'chai';
+import Hotel from '../src/hotel'
+const expect = chai.expect;
+
+describe('Hotel Class', () => {
+  //Declare variable names here
+
+  beforeEach(() => {
+
+  })
+
+
+  describe('Hotel Properties', () => {
+
+
+  })
+
+  describe('Hotel Methods', () => {
+
+    
+  })
+
+
+
+})
