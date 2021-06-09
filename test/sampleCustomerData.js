d7e0e0aff110a422c39c273c839e5b99586b8870
Shawn M (he/him) <shawnmcmahon17@gmail.com>
2021-06-09 11:29:42 -0600

    Add customer sample data

---
+++ test/sampleCustomerData.js
@@ -0,0 +1,19 @@
+const sampleCustomerData = [{
+  {
+    id: 1,
+    name: "Leatha Ullrich"
+  },
+  {
+    id: 2,
+    name: "Rocio Schuster"
+  },
+  {
+    id: 3,
+    name: "Kelvin Schiller"
+  },
+
+
+}]
+
+
+export default sampleCustomerData
