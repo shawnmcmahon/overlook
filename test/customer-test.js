abd93a2dbf5e8d484b9fb385ff5d6e76e95df99d
Shawn M (he/him) <shawnmcmahon17@gmail.com>
2021-06-09 11:21:03 -0600

    Setup customer test structure

---
+++ test/customer-test.js
@@ -0,0 +1,27 @@
+import chai from 'chai';
+import Hotel from '../src/hotel'
+import Customer from '../src/customer'
+const expect = chai.expect;
+
+
+describe('Customer Class', () => {
+  //Declare variable names here
+
+  beforeEach(() => {
+
+  })
+
+
+  describe('Customer Properties', () => {
+
+
+  })
+
+  describe('Customer Methods', () => {
+
+
+  })
+
+
+
+})
