6c3e57e30a47697945dbd757f2bb55d16acde23d
Shawn M (he/him) <shawnmcmahon17@gmail.com>
2021-06-09 22:20:44 -0600

    Correct notation for room and bookings sample data

--- src/hotel.js
+++ src/hotel.js
@@ -1,5 +1,8 @@
 class Hotel {
-  constructor() {
+  constructor(rooms, bookings, customers) {
+    this.rooms = rooms;
+    this.bookings = bookings;
+    this.customers = customers;
 
   }
 }
--- test/customer-test.js
+++ test/customer-test.js
@@ -14,41 +14,43 @@ describe('Customer Class', () => {
   let booking1, booking2, booking3, hotelBookings;
   let hotel;
   beforeEach(() => {
+      customer1 = new Customer(sampleCustomerData.customers[0]);
+      customer2 = new Customer(sampleCustomerData.customers[1]);
+      customer3 = new Customer(sampleCustomerData.customers[2]);
+      hotelCustomers = sampleCustomerData;
+      //May need to instanitate hotelCustomers with classes
+      //of customers rather than just plain customer data
+      // hotelCustomers = {'customers': [
+      //   {
+      //     customer1 = new Customer(sampleCustomerData[0]);
+      //   },
+      //   {
+      //     customer2 = new Customer(sampleCustomerData[1]);
+      //   },
+      //   {
+      //     customer3 = new Customer(sampleCustomerData[2]);
+      //   },
+      // ]};
 
-        customer1 = new Customer(sampleCustomerData[0]);
-        customer2 = new Customer(sampleCustomerData[1]);
-        customer3 = new Customer(sampleCustomerData[2]);
-        hotelcustomers = sampleCustomerData;
-        //May need to instanitate hotelCustomers with classes
-        //of customers rather than just plain customer data
-        // hotelCustomers = {'customers': [
-        //   {
-        //     customer1 = new Customer(sampleCustomerData[0]);
-        //   },
-        //   {
-        //     customer2 = new Customer(sampleCustomerData[1]);
-        //   },
-        //   {
-        //     customer3 = new Customer(sampleCustomerData[2]);
-        //   },
-        // ]};
+      // May be going 1 step too deep in this sample object data set
+      // May effect data once connected to api
+      // May need to figure out how to remove redundant property below
+      room1 = sampleRoomData.sampleRoomData.rooms[0];
+      room2 = sampleRoomData.sampleRoomData.rooms[1];
+      room3 = sampleRoomData.sampleRoomData.rooms[2];
+      hotelRooms = sampleRoomData;
 
-        room1 = sampleRoomData[0];
-        room2 = sampleRoomData[1];
-        room3 = sampleRoomData[2];
-        hotelRooms = sampleRoomData;
+      booking1 = sampleBookingData.sampleBookingData.bookings[0];
+      booking2 = sampleBookingData.sampleBookingData.bookings[1];
+      booking3 = sampleBookingData.sampleBookingData.bookings[2];
+      hotelBookings = sampleBookingData;
 
-        booking1 = sampleBookingData[0];
-        booking2 = sampleBookingData[1];
-        booking3 = sampleBookingData[2];
-        hotelBookings = sampleBookingData;
-
-    hotel = new Hotel(hotelRooms, hotelBookings, sampleCustomerData);
+      hotel = new Hotel(hotelRooms, hotelBookings, sampleCustomerData);
   })
 
 
   describe('Customer Properties', () => {
-    it.only('should be a function', function() {
+    it('should be a function', function() {
       expect(Customer).to.be.a('function');
     });
 
