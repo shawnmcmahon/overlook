e06cdfe7d755e82cd2923e8f169662b3fbc57237
Shawn M (he/him) <shawnmcmahon17@gmail.com>
2021-06-12 14:17:24 -0600

    Add html selectors to prepare for dom manipulation

--- src/scripts.js
+++ src/scripts.js
@@ -8,13 +8,34 @@ import './css/base.scss';
 // import './css/_normalize.scss'
 // import './css/_tablet.scss'
 
+import Customer from './customer';
+import Hotel from './hotel';
+import apiCalls from './apiCalls';
+// import domUpdates from './domUpdates';
 
-import Customer from './customer'
-import Hotel from './hotel'
+// import Images here
 
 
-// An example of how you tell webpack to use an image (also need to link to it in the index.html)
-import './images/turing-logo.png'
-import './images/happyface.png'
+//Page Selectors
+let availableRoomsBackground = document.getElementById('availableRoomsBackground');
+let reservationHistoryBackground = document.getElementById('reservationHistoryBackground');
 
-console.log('This is the JavaScript entry file - your code begins here.');
+
+
+//Form selectors
+let bookDate = document.getElementById('bookDate');
+let bookRoomType = document.getElementById('bookRoomType');
+let bookNumBeds = document.getElementById('bookNumBeds');
+let bookYesBidet = document.getElementById('bookYesBidet');
+let noBidet = document.getElementById('bookNoBidet');
+let doesntMatterBidet = document.getElementById('bookNPBidet');
+let searchButton = document.getElementById('searchRooms');
+let bookButton = document.getElementById('bookRoom');
+
+//Card selectors
+let roomNumber = document.getElementById('roomNumber');
+let roomReservationDate = document.getElementById('roomReservationDate');
+let roomCost = document.getElementById('roomCost');
+let roomType = document.getElementById('roomType');
+let roomNumberOfBeds = document.getElementById('roomNumberOfBeds');
+let roomBidet = document.getElementById('roomBidet');
